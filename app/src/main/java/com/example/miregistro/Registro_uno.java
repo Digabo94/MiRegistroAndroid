63689f4f487203c1c78561652d93a0e5d8a89bbf
Diego G. Morales Lap <digabo94@gmail.com>
2020-10-09 19:51:30 -0700

    Se agrego la actividad Bienvenido, se crearon etiquetas string, se agregaron imagenes

---
+++ app/src/main/java/com/example/miregistro/Registro_uno.java
@@ -0,0 +1,14 @@
+package com.example.miregistro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro_uno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_uno);
+    }
+}
