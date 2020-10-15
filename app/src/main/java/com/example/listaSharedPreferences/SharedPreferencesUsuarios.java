bb20f936ff4a3c042daef0ad8bebe19db8474f47
Diego Gabriel Morales Gallardo <digabo94@gmail.com>
2020-10-14 18:46:33 -0700

    Implementacion del codigo fuente de la actividad modificacion

--- app/src/main/java/com/example/listaSharedPreferences/SharedPreferencesUsuarios.java
+++ app/src/main/java/com/example/listaSharedPreferences/SharedPreferencesUsuarios.java
@@ -2,6 +2,7 @@ package com.example.listaSharedPreferences;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,18 +13,61 @@ public class SharedPreferencesUsuarios{
     public void guardarUsuario(UsuarioDTO usuarioDTO, SharedPreferences preferences, Context context){
          SharedPreferences.Editor editor = preferences.edit();
 
-        editor.putString(usuarioDTO.getPassword(), usuarioDTO.getCorreoElectronico());
-        editor.putString(usuarioDTO.getPassword(), usuarioDTO.getDireccion());
-        editor.putString(usuarioDTO.getPassword(), usuarioDTO.getNombre());
-        editor.putString(usuarioDTO.getPassword(), usuarioDTO.getPassword());
-        editor.putString(usuarioDTO.getPassword(), usuarioDTO.getSexo());
-        editor.putString(usuarioDTO.getPassword(), usuarioDTO.getUsuario());
-        editor.putInt(usuarioDTO.getPassword(), usuarioDTO.getEdad());
-        editor.commit();
+        editor.putString(usuarioDTO.getPassword() + "Correo electrónico", usuarioDTO.getCorreoElectronico());
+        editor.putString(usuarioDTO.getPassword() + "Dirección", usuarioDTO.getDireccion());
+        editor.putString(usuarioDTO.getPassword() + "Nombre", usuarioDTO.getNombre());
+        editor.putString(usuarioDTO.getPassword() + "Contraseña", usuarioDTO.getPassword());
+        editor.putString(usuarioDTO.getPassword() + "Sexo", usuarioDTO.getSexo());
+        editor.putString(usuarioDTO.getPassword() + "Usuario", usuarioDTO.getUsuario());
+        editor.putInt(usuarioDTO.getPassword() + "Edad", usuarioDTO.getEdad());
+        editor.apply();
         Toast.makeText(context,"¡Usuario registrado!", Toast.LENGTH_LONG).show();
     }
 
-    public void modificarUsuario(UsuarioDTO usuarioDTO, SharedPreferences preferences, Context context){
-        preferences.getString(usuarioDTO.getPassword(),"El usuario no existe");
+    public void modificarUsuario(EditText input, String itemSpinner, UsuarioDTO usuarioDTO, SharedPreferences preferences, Context context){
+        String passwordModificacion = null;
+        if(itemSpinner == "Edad") {
+            passwordModificacion = String.valueOf(preferences.getInt(usuarioDTO.getPassword() + itemSpinner, 0));
+        } else {
+            passwordModificacion = preferences.getString(usuarioDTO.getPassword() + itemSpinner, "El usuario no existe");
+        }
+
+       if(passwordModificacion == "El usuario no existe" || passwordModificacion == "0"){
+           Toast.makeText(context, "El usuario no existe",Toast.LENGTH_LONG).show();
+       } else{
+           SharedPreferences.Editor editor = preferences.edit();
+           switch (itemSpinner){
+               case "Correo electrónico":
+                   usuarioDTO.setCorreoElectronico(input.getText().toString());
+                   editor.putString(usuarioDTO.getPassword() + itemSpinner, usuarioDTO.getCorreoElectronico());
+                   break;
+               case "Usuario":
+                   usuarioDTO.setUsuario(input.getText().toString());
+                   editor.putString(usuarioDTO.getPassword() + itemSpinner, usuarioDTO.getUsuario());
+                   break;
+               case "Contraseña":
+                   usuarioDTO.setPassword(input.getText().toString());
+                   editor.putString(usuarioDTO.getPassword() + itemSpinner, usuarioDTO.getPassword());
+                   break;
+               case "Nombre":
+                   usuarioDTO.setNombre(input.getText().toString());
+                   editor.putString(usuarioDTO.getPassword() + itemSpinner, usuarioDTO.getNombre());
+                   break;
+               case "Dirección":
+                   usuarioDTO.setDireccion(input.getText().toString());
+                   editor.putString(usuarioDTO.getPassword() + itemSpinner, usuarioDTO.getDireccion());
+                   break;
+               case "Edad":
+                   usuarioDTO.setEdad(Integer.parseInt(input.getText().toString()));
+                   editor.putInt(usuarioDTO.getPassword() + itemSpinner, usuarioDTO.getEdad());
+                   break;
+               case "Sexo":
+                   usuarioDTO.setSexo(input.getText().toString());
+                   editor.putString(usuarioDTO.getPassword() + itemSpinner, usuarioDTO.getSexo());
+                   break;
+           }
+           editor.apply();
+           Toast.makeText(context,"Se ha modificado exitosamente", Toast.LENGTH_LONG).show();
+       }
     }
 }
--- app/src/main/java/com/example/miregistro/Modificacion.java
+++ app/src/main/java/com/example/miregistro/Modificacion.java
@@ -3,6 +3,7 @@ package com.example.miregistro;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,6 +11,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,12 +21,14 @@ import com.example.listaSharedPreferences.SharedPreferencesUsuarios;
 public class Modificacion extends AppCompatActivity {
 
     Spinner spinnerCampos;
+    TextView sexoModificar;
     EditText input, editTextPass;
     ImageButton regresarNavegacion, confirmarModificacion;
     RadioButton f, m;
     SharedPreferences listaUsuarios;
     SharedPreferencesUsuarios modificarLista;
     UsuarioDTO usuarioDTO;
+    String itemSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,7 @@ public class Modificacion extends AppCompatActivity {
         spinnerCampos = findViewById(R.id.campos);
         input = findViewById(R.id.input);
         editTextPass = findViewById(R.id.editTextPass);
+        sexoModificar = findViewById(R.id.sexoModificar);
         regresarNavegacion = findViewById(R.id.regresarNavegacion);
         confirmarModificacion = findViewById(R.id.confirmarModificacion);
         f = findViewById(R.id.f);
@@ -42,14 +47,55 @@ public class Modificacion extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.array_campos,
                 android.R.layout.simple_spinner_item);
         spinnerCampos.setAdapter(adapter);
+
+        spinnerCampos.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                itemSpinner = parent.getItemAtPosition(position).toString();
+
+                if(itemSpinner == "Sexo"){
+                    sexoModificar.setVisibility(View.VISIBLE);
+                    f.setVisibility(View.VISIBLE);
+                    m.setVisibility(View.VISIBLE);
+                    input.setVisibility(View.INVISIBLE);
+                } else{
+                    sexoModificar.setVisibility(View.INVISIBLE);
+                    f.setVisibility(View.INVISIBLE);
+                    m.setVisibility(View.INVISIBLE);
+                    input.setVisibility(View.VISIBLE);
+                }
+
+                if(itemSpinner == "Edad"){
+                    input.setInputType(InputType.TYPE_CLASS_NUMBER);
+                } else {
+                    input.setInputType(InputType.TYPE_CLASS_TEXT);
+                }
+
+                if(itemSpinner == "Contraseña"){
+                    input.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    input.setInputType(InputType.TYPE_CLASS_TEXT);
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
     }
 
     public void onClickConfirmarModificacion(View view) {
+        usuarioDTO = new UsuarioDTO();
         listaUsuarios = getSharedPreferences("ListaUsuarios", Context.MODE_PRIVATE);
         usuarioDTO.setPassword(editTextPass.getText().toString());
-        modificarLista.modificarUsuario(usuarioDTO, listaUsuarios, getApplicationContext());
+        modificarLista.modificarUsuario(input, itemSpinner, usuarioDTO, listaUsuarios, getApplicationContext());
     }
 
     public void onClickRegresarNavegacion(View view) {
+        finish();
+    }
+
+    public void onClickSpinner(View view) {
+
     }
 }
